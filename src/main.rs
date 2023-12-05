a8af7162389e5037e4789d300a0725c8ce61ef31
Andrés Osorio <andres.osorio@taxfix.de>
2023-12-05 15:54:32 +0100

    bind http server and initial route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::http;
+use axum::routing::{get, Router};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let port = 3000;
+    let address = format!("0.0.0.0:{port}");
+
+    let app = Router::new()
+        .route("/", get(health));
+
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+    Ok(())
+}
+
+async fn health() -> http::StatusCode {
+    http::StatusCode::OK
 }
