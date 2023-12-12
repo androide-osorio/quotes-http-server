ecbce82b88d86ab8a6855a003ec3bcf8e037a865
Andrés Osorio <andres.osorio@taxfix.de>
2023-12-12 12:58:01 +0100

    add create endpoint

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,7 +1,30 @@
 use axum::{http, Json, extract};
-use sqlx::PgPool;
+use serde::{Deserialize, Serialize};
+use sqlx::{PgPool, FromRow};
 
-#[derive(Debug, serde::Deserialize)]
+#[derive(Serialize, FromRow)]
+pub struct Quote {
+    id: uuid::Uuid,
+    book: String,
+    quote: String,
+    inserted_at: chrono::DateTime<chrono::Utc>,
+    updated_at: chrono::DateTime<chrono::Utc>,
+}
+
+impl Quote {
+    pub fn new(quote: String, book: String) -> Self {
+        let now = chrono::Utc::now();
+        Self {
+            id: uuid::Uuid::new_v4(),
+            book,
+            quote,
+            inserted_at: now,
+            updated_at: now,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
 pub struct CreateQuote {
     book: String,
     quote: String,
@@ -14,7 +37,28 @@ pub async fn health() -> http::StatusCode {
 pub async fn create_quote(
     extract::State(pool): extract::State<PgPool>,
     Json(payload): Json<CreateQuote>,
-) -> http::StatusCode {
-    println!("Received request to create quote: {:?}", payload);
-    http::StatusCode::CREATED
+) -> Result<(http::StatusCode, axum::Json<Quote>), http::StatusCode> {
+    let quote = Quote::new(payload.quote, payload.book);
+
+    let res = sqlx::query(
+        r#"
+        INSERT INTO quotes (id, book, quote, inserted_at, updated_at)
+        VALUES ($1, $2, $3, $4, $5)
+        "#,
+    )
+    .bind(&quote.id)
+    .bind(&quote.book)
+    .bind(&quote.quote)
+    .bind(&quote.inserted_at)
+    .bind(&quote.updated_at)
+    .execute(&pool)
+    .await;
+
+    match res {
+        Ok(_) => Ok((http::StatusCode::CREATED, axum::Json(quote))),
+        Err(e) => {
+            eprintln!("Failed to execute query: {}", e);
+            return Err(http::StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    }
 }
